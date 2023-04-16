1a1fb35d2df4684154ab2eb44ddac9e52d3a3c85
Vagif Khalilov <lebronirish@gmail.com>
2023-04-16 16:53:27 +0300

    add partial logic for getUsedVariables

--- src/main/kotlin/inno/ppa/Main.kt
+++ src/main/kotlin/inno/ppa/Main.kt
@@ -2,8 +2,56 @@ package inno.ppa
 
 import WhilelangLexer
 import WhilelangParser
+import WhilelangParser.ProgramContext
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
+
+
+fun getInitializedVariables(statements: List<WhilelangParser.StatementContext>): Set<String> {
+    return emptySet()
+}
+
+
+fun getUsedVariables(statements: List<WhilelangParser.StatementContext>): Set<String> {
+    var usedVariables = emptySet<String>()
+
+    for (statement in statements) {
+        when (statement) {
+            is WhilelangParser.AttribContext ->{
+                //TODO()
+            }
+            is WhilelangParser.WriteContext -> {
+                //TODO()
+            }
+            is WhilelangParser.PrintContext -> {
+                //TODO()
+            }
+            is WhilelangParser.IdContext -> {
+                //TODO()
+            }
+            is WhilelangParser.IfContext -> {
+                //TODO()
+            }
+            is WhilelangParser.WhileContext -> {
+                //TODO()
+            }
+            is WhilelangParser.BlockContext -> {
+                //TODO()
+            }
+            is WhilelangParser.SkipContext -> {
+                //TODO()
+            }
+            is WhilelangParser.SeqStatementContext -> {
+                //TODO()
+            }
+            else -> {
+                throw Exception("Unknown statement type")
+            }
+
+        }
+    }
+}
+
 
 fun main() {
     // Init part
@@ -12,7 +60,8 @@ fun main() {
     val stream = CommonTokenStream(lexer)
     val parser = WhilelangParser(stream)
 
-     parser.program()
+    parser.program()
+    parser.program()
 
     if (parser.numberOfSyntaxErrors == 0) {
         println("The input WHILE program is syntactically correct")
