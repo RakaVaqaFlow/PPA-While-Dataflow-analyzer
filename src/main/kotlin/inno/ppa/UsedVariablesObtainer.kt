d161f2f3301c0142d41d4ac3830f541468486da5
Vagif Khalilov <lebronirish@gmail.com>
2023-04-16 17:29:46 +0300

    add new file

--- src/main/kotlin/inno/ppa/UsedVariablesObtainer.kt
+++ src/main/kotlin/inno/ppa/UsedVariablesObtainer.kt
@@ -1,42 +1,90 @@
 package inno.ppa
 
 import WhilelangParser.*
+import java.beans.Expression
+
 fun getUsedVariables(statementContext: StatementContext): Set<String> {
-    val usedVariables = emptySet<String>()
+    val usedVariables = mutableSetOf<String>()
 
-    val tokens =
-        when (statementContext) {
-            is AttribContext ->{
-                statementContext.expression()
+    when (statementContext) {
+        is AttribContext -> {
+            usedVariables.addAll(getUsedVariables(statementContext.expression()))
 
-            }
-            is WriteContext -> {
-                //TODO()
-            }
-            is PrintContext -> {
-                //TODO()
-            }
-            is IdContext -> {
-                //TODO()
-            }
-            is IfContext -> {
-                //TODO()
-            }
-            is WhileContext -> {
-                //TODO()
-            }
-            is BlockContext -> {
-                //TODO()
-            }
-            is SkipContext -> {
-                //TODO()
-            }
-            is SeqStatementContext -> {
-                //TODO()
-            }
-            else -> {
-                throw Exception("Unknown statement type")
-            }
         }
+
+        is WriteContext -> {
+            //TODO()
+        }
+
+        is PrintContext -> {
+            //TODO()
+        }
+
+        is IdContext -> {
+            //TODO()
+        }
+
+        is IfContext -> {
+            //TODO()
+        }
+
+        is WhileContext -> {
+            //TODO()
+        }
+
+        is BlockContext -> {
+            //TODO()
+        }
+
+        is SkipContext -> {
+            //TODO()
+        }
+
+        is SeqStatementContext -> {
+            //TODO()
+        }
+
+        else -> {
+            throw Exception("Unknown statement type")
+        }
+    }
+    return usedVariables
+}
+
+fun getUsedVariablesInExpression(expression: ExpressionContext): Set<String> {
+    val usedVariables = mutableSetOf<String>()
+    when (expression) {
+        is IdContext -> {
+            usedVariables.add(expression.ID().text)
+        }
+
+        is ReadContext -> {
+            //TODO()
+        }
+
+        is BoolContext -> {
+            //TODO()
+        }
+
+        is IntContext -> {
+            //TODO()
+        }
+
+        is BinOpContext -> {
+            //TODO()
+        }
+
+        is ExpParenContext -> {
+            //TODO()
+        }
+
+        is AndContext -> {
+            //TODO()
+        }
+
+        else -> {
+            throw Exception("Unknown expression type")
+        }
+    }
     return usedVariables
 }
